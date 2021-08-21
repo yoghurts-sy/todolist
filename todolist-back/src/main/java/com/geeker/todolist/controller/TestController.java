a618316b3687349479a539caac9ea2cd476b785e
yoghurts-sy <yoghurts_sy@sina.com>
2021-08-21 16:27:40 +0800

    feat:后端数据库表的搭建，两个表

--- todolist-back/src/main/java/com/geeker/todolist/controller/TestController.java
+++ todolist-back/src/main/java/com/geeker/todolist/controller/TestController.java
@@ -3,11 +3,24 @@ package com.geeker.todolist.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+/**
+ * @author: yoghurt
+ * data:2021-08-21
+ */
 @RestController
 public class TestController {
 
     @GetMapping("/hello")
-    public String Hello() {
-        return "Hello Vue";
+    public List<String> Hello() {
+        List<String> list = new ArrayList<>();
+        list.add("12312");
+        list.add("12312312");
+        list.add("12312312");
+        return list;
     }
 }
