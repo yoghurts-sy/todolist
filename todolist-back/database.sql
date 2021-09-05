c350cea25e5650910fa00aec081fef6f651b0131
ShuFan66 <1209758965@qq.com>
2021-09-05 13:13:33 +0800

    debug in database by sf

--- todolist-back/database.sql
+++ todolist-back/database.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Ultimate v12.09 (64 bit)
-MySQL - 5.5.40 : Database - todolist
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 8.0.13 : Database - todolist
 *********************************************************************
 */
 
@@ -22,15 +22,15 @@ DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_email` varchar(32) NOT NULL,
-  `user_password` varchar(32) NOT NULL,
-  `user_token` varchar(20) NOT NULL,
+  `user_email` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `user_password` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `user_token` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user` */
 
-insert  into `user`(`user_id`,`user_email`,`user_password`,`user_token`) values (1,'123@sina.com','123456','123'),(2,'321@qq.com','321123','321');
+insert  into `user`(`user_id`,`user_email`,`user_password`,`user_token`) values (1,'123@sina.com','123456','123@sina.com?id=1'),(2,'321@qq.com','321123','321@qq.com?id=2'),(3,'final.com','543','final.com?id=12'),(13,'shufan@qq.com','190310','shufan@qq.com?id=13'),(14,'shufan@163.com','123456','shufan@163.com?id=14'),(15,'suyou@163.com','123456','suyou@163.com?id=15'),(16,'1321231@3132.com','123456','a'),(17,'yyq163@163.com','123456','yyq163@163.com?id=17'),(18,'suyouyou@163.com','123456','a'),(19,'suyu@163.com','123456','suyu@163.com?id=19'),(20,'123456789789@qq.com','123456','a');
 
 /*Table structure for table `user_todolist` */
 
